9a473a46aededc0650de2707263a97bde1a7e1a6
ITOAR <itoar@outlook.com>
2022-03-16 23:08:55 +0800

    generalize

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,24 @@
 use plotters::{
     coord::{ranged1d::AsRangedCoord, Shift},
     prelude::*,
-    style::RelativeSize,
+    style::{RelativeSize, SizeDesc},
 };
 use std::{ops::Range, path::Path};
 
 const MIN_BASE: f64 = 10.;
 const MAX_MAINTAIN: u32 = 100;
 
-pub struct GifVisualizer<X = Range<f64>, Y = Range<f64>>
+type GifVisualizer<X = Range<f64>, Y = Range<f64>> = Animator<BitMapBackend<'static>, X, Y>;
+pub struct Animator<DB: DrawingBackend, X = Range<f64>, Y = Range<f64>>
 where
     X: AsRangedCoord,
     Y: AsRangedCoord,
 {
-    gif: DrawingArea<BitMapBackend<'static>, Shift>,
+    draw_area: DrawingArea<DB, Shift>,
     x_range: DrawRange<X>,
     y_range: DrawRange<Y>,
     last_y_range: Option<Y>,
     last_refresh: u32,
-    x_size: u32,
-    y_size: u32,
     caption: Option<String>,
     x_desc: Option<String>,
     y_desc: Option<String>,
@@ -33,18 +32,37 @@ enum DrawRange<A> {
     Static(A),
 }
 
-impl GifVisualizer<Range<f64>, Range<f64>> {
+impl<X, Y> GifVisualizer<X, Y>
+where
+    X: AsRangedCoord,
+    Y: AsRangedCoord,
+{
     pub fn new<T: AsRef<Path>>(path: T, size: (u32, u32), frame_rate: u32) -> Self {
         Self {
-            gif: BitMapBackend::gif(path, size, 1000 / frame_rate)
+            draw_area: BitMapBackend::gif(path, size, 1000 / frame_rate)
                 .expect("creating gif backend")
                 .into_drawing_area(),
             x_range: DrawRange::Auto,
             y_range: DrawRange::Auto,
             last_y_range: None,
             last_refresh: 0,
-            x_size: size.0,
-            y_size: size.1,
+            caption: None,
+            x_desc: None,
+            y_desc: None,
+            x_label_formatter: None,
+            y_label_formatter: None,
+            frame: 0,
+        }
+    }
+}
+impl<DB: DrawingBackend> Animator<DB, Range<f64>, Range<f64>> {
+    pub fn on_backend(back_end: DB) -> Self {
+        Self {
+            draw_area: back_end.into_drawing_area(),
+            x_range: DrawRange::Auto,
+            y_range: DrawRange::Auto,
+            last_y_range: None,
+            last_refresh: 0,
             caption: None,
             x_desc: None,
             y_desc: None,
@@ -174,17 +192,17 @@ impl GifVisualizer<Range<f64>, Range<f64>> {
                 };
                 (Box::new(temp.into_iter()), x_range, y_range)
             };
-        self.gif.fill(&WHITE).expect("filling gif background");
-        let mut ctx = ChartBuilder::on(&self.gif)
-            .margin_right(self.x_size / 50)
-            .x_label_area_size(self.x_size / 10)
-            .y_label_area_size(self.y_size / 10)
+        self.draw_area.fill(&WHITE).expect("filling background");
+        let mut ctx = ChartBuilder::on(&self.draw_area)
+            .margin_right(0.02.percent_width())
+            .x_label_area_size(0.1.percent_height())
+            .y_label_area_size(0.1.percent_width())
             .caption(
                 self.caption
                     .as_ref()
                     .map(|x| format!("{} {}", x, self.frame))
                     .unwrap_or_else(|| self.frame.to_string()),
-                ("sans-serif", self.y_size / 20),
+                ("sans-serif", 0.05.percent_height()),
             )
             .build_cartesian_2d(
                 x_range,
@@ -199,8 +217,8 @@ impl GifVisualizer<Range<f64>, Range<f64>> {
             )
             .expect("building chart context");
         let mut mesh = ctx.configure_mesh();
-        mesh.x_label_style(("sans-serif", self.x_size / 40))
-            .y_label_style(("sans-serif", self.y_size / 40))
+        mesh.x_label_style(("sans-serif", 0.025.percent_height()))
+            .y_label_style(("sans-serif", 0.025.percent_width()))
             .y_labels(12);
         if let Some(ref s) = self.x_desc {
             mesh.x_desc(s);
@@ -214,28 +232,24 @@ impl GifVisualizer<Range<f64>, Range<f64>> {
         if let Some(ref f) = self.y_label_formatter {
             mesh.y_label_formatter(f);
         }
-        mesh.axis_style(BLACK.stroke_width(self.x_size / 1000 + 1))
+        mesh.axis_style(BLACK.stroke_width(1e-3.percent().in_pixels(&self.draw_area) as u32 + 1))
             .draw()
             .expect("drawing mesh");
         ctx.draw_series(LineSeries::new(v, RED))
             .expect("plotting points");
-        self.gif.present().expect("flushing current frame");
+        self.draw_area.present().expect("flushing current frame");
         self.frame += 1;
     }
 }
 
 pub struct ColorMapVisualizer<
-    P,
+    DB: DrawingBackend,
     B = f64,
     XF: Fn(&usize) -> String = fn(&usize) -> String,
     YF: Fn(&usize) -> String = fn(&usize) -> String,
-> where
-    P: AsRef<Path>,
-{
-    path: P,
+> {
+    draw_area: DrawingArea<DB, Shift>,
     matrix: Vec<Vec<B>>,
-    x_size: u32,
-    y_size: u32,
     color_range: DrawRange<Range<B>>,
     caption: Option<String>,
     x_desc: Option<String>,
@@ -245,13 +259,11 @@ pub struct ColorMapVisualizer<
     auto_range: Option<Range<B>>,
 }
 
-impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize) -> String> {
-    pub fn new(path: P, size: (u32, u32)) -> Self {
+impl<'a> ColorMapVisualizer<BitMapBackend<'a>, f64, fn(&usize) -> String, fn(&usize) -> String> {
+    pub fn new<P: AsRef<Path>>(path: &'a P, size: (u32, u32)) -> Self {
         Self {
-            path,
+            draw_area: BitMapBackend::new(path, size).into_drawing_area(),
             matrix: Vec::new(),
-            x_size: size.0,
-            y_size: size.1,
             color_range: DrawRange::Auto,
             caption: None,
             x_desc: None,
@@ -261,6 +273,8 @@ impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize)
             auto_range: None,
         }
     }
+}
+impl<DB: DrawingBackend> ColorMapVisualizer<DB, f64, fn(&usize) -> String, fn(&usize) -> String> {
     pub fn set_color_range(&mut self, x: Range<f64>) -> &mut Self {
         self.color_range = DrawRange::Static(x);
         self
@@ -319,17 +333,18 @@ impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize)
             1.
         };
         let color_map = |v| ((v - range_min) / range);
-        let area = BitMapBackend::new(&self.path, (self.x_size, self.y_size)).into_drawing_area();
-        area.fill(&WHITE).expect("filling area background");
-        let (area, bar) = area.split_horizontally(RelativeSize::Width(0.85));
+        self.draw_area
+            .fill(&WHITE)
+            .expect("filling area background");
+        let (area, bar) = self.draw_area.split_horizontally(RelativeSize::Width(0.85));
         let mut builder = ChartBuilder::on(&area);
         builder
-            .margin_right(self.x_size / 50)
-            .margin_top(self.y_size / 50)
-            .y_label_area_size(self.x_size / 10)
-            .x_label_area_size(self.y_size / 10);
+            .margin_right(0.02.percent_width())
+            .margin_top(0.02.percent_height())
+            .y_label_area_size(0.1.percent_width())
+            .x_label_area_size(0.1.percent_height());
         if let Some(s) = self.caption {
-            builder.caption(s, ("sans-serif", self.y_size / 40));
+            builder.caption(s, ("sans-serif", 0.025.percent_height()));
         }
         let row_len = self
             .matrix
@@ -344,8 +359,8 @@ impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize)
             )
             .expect("building chart context");
         let mut mesh = chart.configure_mesh();
-        mesh.x_label_style(("sans-serif", self.y_size / 40))
-            .y_label_style(("sans-serif", self.x_size / 40))
+        mesh.x_label_style(("sans-serif", 0.025.percent_height()))
+            .y_label_style(("sans-serif", 0.025.percent_width()))
             .disable_x_mesh()
             .disable_y_mesh();
         if let Some(ref s) = self.x_desc {
@@ -385,10 +400,10 @@ impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize)
 
         let mut builder = ChartBuilder::on(&bar);
         builder
-            .margin_right(self.x_size / 50)
-            .margin_top(self.y_size / 50)
-            .y_label_area_size(self.x_size / 10)
-            .x_label_area_size(self.y_size / 10);
+            .margin_right(0.02.percent_width())
+            .margin_top(0.02.percent_height())
+            .y_label_area_size(0.01.percent_width())
+            .x_label_area_size(0.01.percent_height());
         let mut chart = builder
             .build_cartesian_2d((0.)..1., range_min..range_max)
             .expect("building colorbar");
@@ -397,7 +412,7 @@ impl<P: AsRef<Path>> ColorMapVisualizer<P, f64, fn(&usize) -> String, fn(&usize)
         mesh.disable_x_mesh()
             .disable_y_mesh()
             .disable_x_axis()
-            .y_label_style(("sans-serif", self.x_size / 40));
+            .y_label_style(("sans-serif", 0.025.percent_width()));
         mesh.draw().expect("drawing colorbar mesh");
         chart
             .draw_series(
--- tests/colormap.rs
+++ tests/colormap.rs
@@ -6,7 +6,7 @@ use jkplot::ColorMapVisualizer;
 fn colo_map() {
     let mut path = PathBuf::from(env!("CARGO_TARGET_TMPDIR"));
     path.push("color_map.png");
-    let mut c = ColorMapVisualizer::new(path, (1000, 1000));
+    let mut c = ColorMapVisualizer::new(&path, (1000, 1000));
     for i in 1..10 {
         c.push(vec![i as f64; 10]);
     }
